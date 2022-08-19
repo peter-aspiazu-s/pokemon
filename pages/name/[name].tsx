08b5bd101c15f2dfa447dc6ad58a1032bebd66c4
Peter Aspiazu <paspiazusabando@gmail.com>
2022-08-19 17:12:43 -0500

    Agregando isg icremental static generation

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -116,7 +116,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonNames.map(name => ({
       params: { name }
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -125,10 +125,22 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
   
   const {name} = params as {name: string};
 
+  const pokemon = await getPokemonInfo(name);
+
+  if(!pokemon){
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name)
-    }
+      pokemon
+    },
+    revalidate: 86400,
   }
 }
 
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -114,7 +114,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemon151.map(id => ({
       params: { id }
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -123,12 +123,22 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
   
   const {id} = params as {id: string};
 
+  const pokemon = await getPokemonInfo(id);
   
+  if(!pokemon){
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   return {
     props: {
-      pokemon: await getPokemonInfo(id)
-    }
+      pokemon
+    },
+    revalidate: 86400,
   }
 }
 
